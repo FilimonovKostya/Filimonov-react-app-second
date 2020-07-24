9317b43f977d42f89611d2c77368e3d2677e213c
Konstantin Filimonov <58990191+FilimonovKostya@users.noreply.github.com>
2020-07-24 14:04:26 +0300

    make first homework

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,26 +1,35 @@
 import React from "react";
+import Message from "./Message";
 
-const messageData = {
+ const messageData = {
     avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
     name: "Some Name",
     message: "some text",
     time: "22:00",
 };
 
-function HW1() {
+export type MessageType = {
+     avatar: string
+     name:string
+     message:string
+     time:string
+ }
+
+
+ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
+            home works 1
 
-            {/*should work (должно работать)*/}
+            should work (должно работать)
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import {MessageType} from "./HW1";
+import s from './Message.module.css'
 
-function Message() {
+function Message(props: MessageType) {
     return (
         <div>
+            <div className={s.circle}>
+                <img src={props.avatar} alt="avatar"/>
+                <div className={s.envelope}>
+                   <p  >{props.name}</p>
+                    <p>{props.message}</p>
+                    <div className={s.time}>
+                        <span>{props.time}</span>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
