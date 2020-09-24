4724ad26ef8db954317f8f006982cfabe9b29198
xrani <konstantinfilimonou@gmail.com>
2020-09-24 10:04:36 +0300

    Updates

---
+++ precache-manifest.fc35d53c21083864e098c1eeaab8d2e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe5100b0cf4608d3f6416fe9377e79c",
+    "url": "/Filimonov-react-app-second/index.html"
+  },
+  {
+    "revision": "6a882dea569fa80aafec",
+    "url": "/Filimonov-react-app-second/static/css/main.e2037209.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/Filimonov-react-app-second/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Filimonov-react-app-second/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a882dea569fa80aafec",
+    "url": "/Filimonov-react-app-second/static/js/main.8a368f87.chunk.js"
+  },
+  {
+    "revision": "575d2bb9639603e2e9b1",
+    "url": "/Filimonov-react-app-second/static/js/runtime-main.b87bd014.js"
+  }
+]);
