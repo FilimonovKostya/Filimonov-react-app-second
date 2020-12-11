c1b622911858e466e2ef1c62fadc6562d31d2409
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-12-11 14:28:16 +0300

    Начало положено _)

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HW13 = () => {
+    return <div>Test</div>
+}
+
+export default HW13
---
+++ src/p2-homeworks/h13/Request.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Request = () => {
+    return <div>
+        <input type="checkbox"/>
+        <button>Xz</button>
+    </div>
+}
+
+export default Request
