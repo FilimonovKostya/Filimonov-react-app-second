87c5a05a0574ea8cc76acf86e6a89482f8b4545a
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-12-15 23:37:39 +0300

    Спасибо за домашки (づ｡◕‿‿◕｡)づ

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import Request from './Request'
 const HW13 = () => {
-    return <div>Test</div>
+    return <Request/>
 }
 
 export default HW13
--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {authAPI} from "./RequestsAPI";
 
 const Request = () => {
+
+    const [text, setText] = React.useState<string>('')
+    const [fakeError, setError] = React.useState<string>('')
+    const [showError, setShowError] = React.useState<boolean>(false)
+
+    useEffect(() => {
+        authAPI(showError)
+            .then(res => setText(res.data.errorText))
+            .catch(error => {
+                setError(error.response ? error.response.data.errorText : error.message)
+            })
+    })
+
+    const onChangeErrorClick = () => setShowError(!showError)
+
     return <div>
         <input type="checkbox"/>
-        <button>Xz</button>
+        <button onClick={onChangeErrorClick}>Press me</button>
+        <div> Снизу должно что-то быть (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</div>
+        <h4>
+            {showError
+                ? text
+                : fakeError
+            }
+        </h4>
     </div>
 }
 
